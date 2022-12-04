0a130c3ae4095782bc13a42f7a3383f35f3d51b8
Ignacio Molina <ignacio.molina@alumnos.ucn.cl>
2022-12-04 16:42:51 -0300

    Add Taller3_Resources

---
+++ Assets/Taller3_Resources/Arduac/Arduac.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Collections;
+
+public class Arduac : MonoBehaviour
+{
+    [Header("Arduac Behaviour")]
+    [SerializeField] string defaultSlogan = "No soy un cubo genérico";
+}
